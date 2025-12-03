6512615a875988f42b83955663c3e39da74ab2b9
betflop <pawwlyk52@gmail.com>
2025-12-03 20:35:29 +0300

    lsp

--- lazyvim/nvim/lua/config/keymaps.lua
+++ lazyvim/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>xi", function()
+  -- получаем текущее состояние virtual_text
+  local current = vim.diagnostic.config().virtual_text
+  -- переключаем на противоположное
+  vim.diagnostic.config({ virtual_text = not current })
+  print("Virtual text:", not current and "Включён" or "Отключён")
+end, { desc = "Toggle inline LSP errors" })
